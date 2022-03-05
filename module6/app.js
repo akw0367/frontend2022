e9c2959ff6978df90da788463ba3b5b8158ff77a
Alina Wieprecht <akwieprecht@gmail.com>
2022-03-05 14:30:44 -0500

    working base version

--- module6/app.js
+++ module6/app.js
@@ -1,10 +1,33 @@
 (function () {
 'use strict';
 
-angular.module('myFirstApp', [])
+// attaching controller to the module specified in html
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
 
-.controller('MyFirstController', function () {
+// specifically attaching scope and filter objects to be injected into controller
+// protects from minification b/c string literal in array
+LunchCheckController.$inject = ['$scope', '$filter'];
 
-});
+function LunchCheckController($scope, $filter) {
+    $scope.msg = "";
+    $scope.lunchList = "";
+
+    $scope.btnClick = function() {
+        var myList = $scope.lunchList;
+        var myMsg = "";
+        if (myList == "") {
+            myMsg = "Please enter data first";
+        } else {
+            var myArr = myList.split(",");
+            if (myArr.length <= 3) {
+                myMsg = "Enjoy!"
+            } else {
+                myMsg = "Too much!";
+            }
+        }
+        $scope.msg = myMsg;
+    }
+}
 
 })();
