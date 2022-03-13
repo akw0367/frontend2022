7e6d8b42d2314502c244440935b7b3a3db2357c8
Alina Wieprecht <akwieprecht@gmail.com>
2022-03-13 16:51:26 -0400

    all reqs done

--- module7/app.js
+++ module7/app.js
@@ -15,7 +15,6 @@ function ToBuyController(ShoppingListCheckOffService) {
     tbList.buyItem = function (itemIndex) {
         ShoppingListCheckOffService.buyItem(itemIndex)
     }
-
   
 }
 
@@ -32,14 +31,40 @@ function ShoppingListCheckOffService() {
   var service = this;
 
   // List of shopping items
-  var itemsToBuy = ['a', 'b', 'c', 'd','e'];
+  var itemsToBuy = [
+        {
+          name: 'apple', 
+          pricePerItem: 1,
+          quantity: 1
+        },
+        {
+        name: 'beans', 
+        pricePerItem: 2,
+        quantity: 1
+        },
+        {
+            name: 'cherries', 
+            pricePerItem: 3,
+            quantity: 1
+        },
+        {
+            name: 'danish', 
+            pricePerItem: 4,
+            quantity: 1
+        },
+        {
+            name: 'eggs', 
+            pricePerItem: 5,
+            quantity: 1
+        },
+      
+      ];
   var itemsBought = [];
 
   service.buyItem = function (itemIndex) {
     var i = itemsToBuy[itemIndex];
     itemsToBuy.splice(itemIndex, 1);
     itemsBought.push(i);
-
   };
 
   service.getItemsToBuy = function () {
