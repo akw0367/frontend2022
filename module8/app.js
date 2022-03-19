60c57998ba69eedf5723c90ec92856c9a5049494
Alina Wieprecht <akwieprecht@gmail.com>
2022-03-19 13:36:52 -0400

    http query and filtering

--- module8/app.js
+++ module8/app.js
@@ -3,17 +3,44 @@
     
     angular.module('NarrowItDownApp', [])
     .controller('NarrowItDownController', NarrowItDownController)
-    .service('MenuSearchService', MenuSearchService);
+    .service('MenuSearchService', MenuSearchService)
+    .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
     
     NarrowItDownController.$inject = ['MenuSearchService'];
     function NarrowItDownController(MenuSearchService) {
 
+        var nitc = this;
+
+        nitc.searchTerm = "";
+
+        nitc.getMatchedMenuItems = function () {
+            MenuSearchService.getMatchedMenuItems(nitc.searchTerm);
+        }
     }
     
-    function MenuSearchService() {
+    MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+    function MenuSearchService($http, ApiBasePath) {
       var service = this;
     
       service.getMatchedMenuItems = function (searchTerm) {
+        console.log(searchTerm);
+        return $http({
+            method: "GET",
+            url: (ApiBasePath + "/menu_items.json")
+          }).then(function (result) {
+
+            console.log(result);
+            // process result and only keep items that match
+            var foundItems = result.data.menu_items.filter((item) => {
+                console.log(item.name);
+                return item.name.toLowerCase().includes(searchTerm.toLowerCase());
+            });;
+
+            console.log(foundItems);
+        
+            // return processed items
+            return foundItems;
+        });
       };
     }
     
