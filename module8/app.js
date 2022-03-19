2135c810bc0f6f454998f881af4d3e7fdcf4a7e1
Alina Wieprecht <akwieprecht@gmail.com>
2022-03-19 12:42:21 -0400

    mod 8 starter

---
+++ module8/app.js
@@ -0,0 +1,21 @@
+(function () {
+    'use strict';
+    
+    angular.module('NarrowItDownApp', [])
+    .controller('NarrowItDownController', NarrowItDownController)
+    .service('MenuSearchService', MenuSearchService);
+    
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController(MenuSearchService) {
+
+    }
+    
+    function MenuSearchService() {
+      var service = this;
+    
+      service.getMatchedMenuItems = function (searchTerm) {
+      };
+    }
+    
+    })();
+    
